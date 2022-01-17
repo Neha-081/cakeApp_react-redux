3ecb36bddf69320c169de08a0692ed0ee68a5f92
Neha <nehaprajapati1150@gmail.com>
2022-01-17 13:51:54 +0530

    connect with hooks

---
+++ src/components/HooksCakeContainer.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { useSelector } from 'react-redux'
+import { useDispatch } from 'react-redux'
+
+function HooksCakeContainer() {
+ const numOfCakes = useSelector(state=>state.numOfCakes)
+ const dispatch=useDispatch()   
+ return (
+        <div>
+            <h2>Num of cakes : {numOfCakes}</h2>
+            <button onClick={()=>dispatch()}>buy cakes</button>
+        </div>
+    )
+}
+
+export default HooksCakeContainer
